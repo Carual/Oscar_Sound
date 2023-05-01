8fcfee0a180fa10fd465836af29df3a0b4724da6
Carual <juan.sartru@gmail.com>
2023-05-01 10:01:15 -0500

    Update analog_a0.ino

--- Arduino/analog_a0/analog_a0.ino
+++ Arduino/analog_a0/analog_a0.ino
@@ -1,19 +1,20 @@
-void setup() { 
-  Serial.begin(9600); //Inicializamos monitor serie para visualizar los valores de LDR. 
+void setup(){
+  Serial.begin(9600); // Inicializamos monitor serie para visualizar los valores de LDR.
 }
 
-void loop() { 
-  ; //Leemos el valor del pinLDR y lo guardamos en la variable creada.
-  Serial.print(int (analogRead(A0)/10.33));
+void loop()
+{
+  ; // Leemos el valor del pinLDR y lo guardamos en la variable creada.
+  Serial.print(int(99 - (analogRead(A0) / 10.33)));
   Serial.print(",");
-  Serial.print(int (analogRead(A1)/10.33));
+  Serial.print(int(99 - (analogRead(A1) / 10.33)));
   Serial.print(",");
-  Serial.print(int (analogRead(A2)/10.33));
+  Serial.print(int(99 - (analogRead(A2) / 10.33)));
   Serial.print(",");
-  Serial.print(int (analogRead(A3)/10.33));
+  Serial.print(int(99 - (analogRead(A3) / 10.33)));
   Serial.print(",");
-  Serial.print(int (analogRead(A4)/10.33));
+  Serial.print(int(99 - (analogRead(A4) / 10.33)));
   Serial.print(",");
-  Serial.println(int (analogRead(A5)/10.33));
- delay(100);
+  Serial.println(int(99 - (analogRead(A5) / 10.33)));
+  delay(100);
 }
